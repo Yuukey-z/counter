017cbb1bf017397666271118d728040a1a6cc292
Yuukey-z <rizkymaouu@gmail.com>
2026-02-02 22:47:12 +0800

    counter now can be used.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("incraseBtn");
+const increaseBtn = document.getElementById("increaseBtn");
 const labelCount = document.getElementById("labelCount");
 let count = 0;
 
 increaseBtn.onclick = function(){
     count++;
-    document.getElementById("labelCount").textContent = count;
+    labelCount.textContent = count;
 }
 decreaseBtn.onclick = function(){
     count--;
-    document.getElementById("labelCount").textContent = count;
+    labelCount.textContent = count;
 }
 resetBtn.onclick = function(){
     count = 0;
-    document.getElementById("labelCount").textContent = count;
+    labelCount.textContent = count;
 }
 
